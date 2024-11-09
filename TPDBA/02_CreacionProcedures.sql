041c50781b97752f9b5155c2ee7f3114f5dbb58e
Lucas <lukybrando2003@gmail.com>
2024-11-08 22:25:04 -0300

    Pasaje de v2 a main

--- TPDBA/02_CreacionProcedures.sql
+++ TPDBA/02_CreacionProcedures.sql
@@ -1,6 +1,20 @@
 USE Com5600G01
 GO
 
+--Para el .xlsx:
+--Para cargar los XLSX tuvimos que cambiar algo de los permisos de windows.
+--Se busca services.msc, dentro de ese programa se busca SQL SERVER(SQLEXPRESS), 
+--click derecho propiedades, pestania inicio sesion y seleccionar Cuenta del sistema local(o algo parecido)
+--Antes, para que funcione este SP:
+sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE;
+GO
+sp_configure 'Ad Hoc Distributed Queries', 1;
+GO
+RECONFIGURE;
+GO
+
 CREATE OR ALTER FUNCTION Procedimientos.ArreglarLetras(@str NVARCHAR(100)) RETURNS NVARCHAR(100)
 BEGIN
 
@@ -18,22 +32,37 @@ GO
 
 --Para insertar los datos de los archivos
 --Para el .csv:
-CREATE OR ALTER PROCEDURE Procedimientos.CargarCSV
+--Archivo de Catalogo:
+CREATE OR ALTER PROCEDURE Procedimientos.CargarCatalogo
     @direccion VARCHAR(255),					-- Parámetro para la ruta del archivo
-    @terminator CHAR(1),						-- Delimitador de campo
-    @tabla VARCHAR(50)							-- Nombre de la tabla con esquema
+    @terminator CHAR(1)							-- Delimitador de campo
 AS
 BEGIN
-    DECLARE @sql NVARCHAR(500); 
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT * FROM tempdb.sys.objects WHERE name = '##CatalogoTemp' AND type = 'U')
+	BEGIN
+		CREATE TABLE ##CatalogoTemp (
+			Id VARCHAR(10),              
+			Categoria VARCHAR(100),
+			Nombre NVARCHAR(100),
+			Precio VARCHAR(10),          
+			Precio_Ref VARCHAR(10),      
+			Unidad_Ref VARCHAR(10),
+			Fecha VARCHAR(20)        
+		);
+	END;
+
+    DECLARE @sql NVARCHAR(MAX); 
 
     SET @sql = N'
-    BULK INSERT ' + @tabla + '
+    BULK INSERT ##CatalogoTemp
     FROM ''' + @direccion + '''
     WITH (
         FIELDTERMINATOR = ''' + @terminator + ''',
-        ROWTERMINATOR= ''0x0A'',
-        CODEPAGE = ''65001'',
-        FIRSTROW = 2,
+        ROWTERMINATOR = ''0x0A'',       
+        CODEPAGE = ''65001'',           
+        FIRSTROW = 2,                   
         FORMAT = ''CSV''
     );';
 
@@ -41,60 +70,112 @@ BEGIN
 END;
 GO
 
---Para el .xlsx:
---Para cargar los XLSX tuvimos que cambiar algo de los permisos de windows.
---Se busca services.msc, dentro de ese programa se busca SQL SERVER(SQLEXPRESS), 
---click derecho propiedades, pestania inicio sesion y seleccionar Cuenta del sistema local(o algo parecido)
---Antes, para que funcione este SP:
-sp_configure 'show advanced options', 1;
-GO
-RECONFIGURE;
-GO
-sp_configure 'Ad Hoc Distributed Queries', 1;
-GO
-RECONFIGURE;
-GO
-
-CREATE OR ALTER PROCEDURE Procedimientos.CargarImportados
-    @direccion VARCHAR(100),
-    @tabla VARCHAR(100),
-    @pagina VARCHAR(100)
+--Archivo de Ventas_registradas:
+CREATE OR ALTER PROCEDURE Procedimientos.CargarHistorialCSV
+    @direccion VARCHAR(255),    
+    @terminator CHAR(1)        
 AS
 BEGIN
+    IF NOT EXISTS (SELECT * FROM tempdb.sys.objects WHERE name = '##Historial' AND type = 'U')
+    BEGIN
+        CREATE TABLE ##Historial (
+            IdFactura VARCHAR(11),
+            TipoFactura VARCHAR(1),
+            Ciudad VARCHAR(100),
+            TipoCliente VARCHAR(10),
+            Genero VARCHAR(6),
+            Producto VARCHAR(100),
+            PrecioUni VARCHAR(10),
+            Cantidad VARCHAR(10),
+            Fecha VARCHAR(15),
+            Hora VARCHAR(10),
+            MedioPago VARCHAR(30),
+            Empleado VARCHAR(10),
+            IdPago VARCHAR(30)
+        );
+    END;
+
     DECLARE @sql NVARCHAR(MAX);
-    -- Inserta los datos en la tabla temporal global
+
     SET @sql = N'
-    INSERT INTO ' + QUOTENAME(@tabla) + N'
-    SELECT 
-        CAST(IdProducto AS INT),
-        CAST(NombreProducto AS NVARCHAR(100)),
-        CAST(Proveedor AS VARCHAR(100)),
-        CAST([Categoría] AS VARCHAR(50)),
-        CAST(CantidadPorUnidad AS VARCHAR(50)),
-        CAST(REPLACE(REPLACE(PrecioUnidad, ''$'', ''''), '' '', '''') AS DECIMAL(6,2))
-    FROM OPENROWSET(
-        ''Microsoft.ACE.OLEDB.12.0'',
-        ''Excel 12.0; Database=' + @direccion + '; HDR=YES;'', 
-        ''SELECT * FROM [' + @pagina + N'$]'' 
+    BULK INSERT ##Historial
+    FROM ''' + @direccion + '''
+    WITH (
+        FIELDTERMINATOR = ''' + @terminator + ''',   
+        ROWTERMINATOR = ''0x0A'',                    
+        CODEPAGE = ''65001'',                        
+        FIRSTROW = 2,                               
+        FORMAT = ''CSV''
     );';
 
     EXEC sp_executesql @sql;
 END;
 GO
 
+--Para los .xlsx:
+--Archivo de Productos_importados:
+CREATE OR ALTER PROCEDURE Procedimientos.CargarImportados
+    @direccion VARCHAR(100),                
+    @pagina VARCHAR(100)                    
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	IF NOT EXISTS (SELECT * FROM tempdb.sys.objects WHERE name = '##ProductosImportados' AND type = 'U')
+	BEGIN
+		CREATE TABLE ##ProductosImportados (
+			IdProducto VARCHAR(10),             
+			Nombre NVARCHAR(100),
+			Proveedor VARCHAR(100),
+			Categoria VARCHAR(50),
+			CantidadPorUnidad VARCHAR(50),
+			PrecioUnidad VARCHAR(10)             
+		);
+	END;
+
+    DECLARE @sql NVARCHAR(MAX);
+    SET @sql = N'
+    INSERT INTO ##ProductosImportados
+    SELECT 
+        CAST(IdProducto AS VARCHAR(10)),                
+        CAST(NombreProducto AS NVARCHAR(100)),
+        CAST(Proveedor AS VARCHAR(100)),
+        CAST([Categoría] AS VARCHAR(50)),
+        CAST(CantidadPorUnidad AS VARCHAR(50)),
+        CAST(REPLACE(REPLACE(PrecioUnidad, ''$'', ''''), '' '', '''') AS VARCHAR(10)) 
+    FROM OPENROWSET(
+        ''Microsoft.ACE.OLEDB.12.0'',
+        ''Excel 12.0; Database=' + QUOTENAME(@direccion, '''') + '; HDR=YES;'', 
+        ''SELECT * FROM [' + QUOTENAME(@pagina, '''') + '$]''
+    );';
+
+    EXEC sp_executesql @sql;
+END;
+GO
+
+--Archivo de Electronic Accessories:
 CREATE OR ALTER PROCEDURE Procedimientos.CargarElectronic
     @direccion VARCHAR(100),
-    @tabla VARCHAR(100),
     @pagina VARCHAR(100)
 AS
 BEGIN
+	SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT * FROM tempdb.sys.objects WHERE name = '##ElectronicAccessories' AND type = 'U')
+    BEGIN
+        CREATE TABLE ##ElectronicAccessories (
+            Nombre NVARCHAR(100),
+            PrecioUSD VARCHAR(10) 
+        );
+    END;
+
     DECLARE @sql NVARCHAR(MAX);
-	--Inserta los datos en la tabla temporal global
+
     SET @sql = N'
-    INSERT INTO ' + QUOTENAME(@tabla) + N'
+    INSERT INTO ##ElectronicAccessories (Nombre, PrecioUSD)
     SELECT 
         CAST(Product AS NVARCHAR(100)),
-        CAST([Precio Unitario en dolares] AS DECIMAL(6,2))
+        CAST(REPLACE([Precio Unitario en dolares], ''$'', '''') AS DECIMAL(6,2))
     FROM OPENROWSET(
         ''Microsoft.ACE.OLEDB.12.0'',
         ''Excel 12.0; Database=' + @direccion + '; HDR=YES;'', 
@@ -103,19 +184,18 @@ BEGIN
 
     EXEC sp_executesql @sql;
 END;
-GO	
+GO
 
+--Archivo Informacion_Complementaria / Clasificacion Productos:
 CREATE OR ALTER PROCEDURE Procedimientos.CargarClasificacion
-    @direccion VARCHAR(100),
-    @tabla VARCHAR(100),
-    @pagina VARCHAR(100),
-    @esquema VARCHAR(20)
+    @direccion VARCHAR(100),       
+    @pagina VARCHAR(100)           
 AS
 BEGIN
     DECLARE @sql NVARCHAR(MAX);
-	--Inserta los datos
+
     SET @sql = N'
-    INSERT INTO ' + QUOTENAME(@esquema) + N'.' + QUOTENAME(@tabla) + N'
+    INSERT INTO Complementario.CategoriaDeProds (LineaDeProducto, Producto)
     SELECT 
         CAST([Línea de producto] AS VARCHAR(100)),
         CAST(Producto AS VARCHAR(100))
@@ -129,79 +209,50 @@ BEGIN
 END;
 GO
 
+--Archivo Informacion_Complementaria / Empleados:
 CREATE OR ALTER PROCEDURE Procedimientos.CargarEmpleados
     @direccion VARCHAR(100),
-    @tabla VARCHAR(100),
-    @pagina VARCHAR(100),
-    @esquema VARCHAR(20),
-	@FraseClave NVARCHAR(128)
+    @pagina VARCHAR(100)
 AS
 BEGIN
     DECLARE @sql NVARCHAR(MAX);
-	--Inserta los datos
+
     SET @sql = N'
-	INSERT INTO ' + QUOTENAME(@esquema) + N'.' + QUOTENAME(@tabla) + N' (
-		Legajo,
-		Nombre,
-		Apellido,
-		DNI,
-		Direccion,
-		EmailPersonal,
-		EmailEmpresa,
-		CUIL,
-		Cargo,
-		Sucursal,
-		Turno,
-		EstaActivo,
-		DNI_Cifrado,
-		Direccion_Cifrada,
-		EmailPersonal_Cifrado,
-		CUIL_Cifrado
-	)
-	SELECT 
-		CAST([Legajo/ID] AS INT),
-		CAST(Nombre AS VARCHAR(50)),
-		CAST(Apellido AS VARCHAR(50)),
-		CAST(DNI AS INT),
-		CAST(Direccion AS VARCHAR(200)),
-		CAST([email personal] AS VARCHAR(100)),
-		CAST([email empresa] AS VARCHAR(100)),
-		CAST(CUIL AS VARCHAR(11)),
-		CAST(Cargo AS VARCHAR(50)),
-		CAST(Sucursal AS VARCHAR(100)),
-		CAST(Turno AS VARCHAR(25)),
-		1,                   -- EstaActivo (asignado como 1 por defecto)
-		NULL,                -- DNI_Cifrado
-		NULL,                -- Direccion_Cifrada
-		NULL,                -- EmailPersonal_Cifrado
-		NULL                 -- CUIL_Cifrado
-	FROM OPENROWSET(
-		''Microsoft.ACE.OLEDB.12.0'',
-		''Excel 12.0; Database=' + @direccion + '; HDR=YES;'', 
-		''SELECT * FROM [' + @pagina + N'$] WHERE [Legajo/ID] IS NOT NULL''
-	);';
+    INSERT INTO Complementario.Empleados (Legajo,Nombre,Apellido,DNI,Direccion,EmailPersonal,EmailEmpresa,CUIL,Cargo,Sucursal,Turno,EstaActivo)
+    SELECT 
+        CAST([Legajo/ID] AS INT),
+        CAST(Nombre AS VARCHAR(50)),
+        CAST(Apellido AS VARCHAR(50)),
+        CAST(DNI AS INT),
+        CAST(Direccion AS VARCHAR(200)),
+        CAST([email personal] AS VARCHAR(100)),
+        CAST([email empresa] AS VARCHAR(100)),
+        CAST(CUIL AS VARCHAR(11)),
+        CAST(Cargo AS VARCHAR(50)),
+        CAST(Sucursal AS VARCHAR(100)),
+        CAST(Turno AS VARCHAR(25)),
+        1  
+    FROM OPENROWSET(
+        ''Microsoft.ACE.OLEDB.12.0'',
+        ''Excel 12.0; Database=' + @direccion + '; HDR=YES;'', 
+        ''SELECT * FROM [' + @pagina + N'$] WHERE [Legajo/ID] IS NOT NULL''
+    );
+    ';
 
-	EXEC sp_executesql @sql;
-
-	UPDATE Complementario.Empleados
-    SET DNI_Cifrado = EncryptByPassPhrase(@FraseClave, CONVERT(NVARCHAR(12), DNI)),
-        Direccion_Cifrada = EncryptByPassPhrase(@FraseClave, Direccion),
-        EmailPersonal_Cifrado = EncryptByPassPhrase(@FraseClave, EmailPersonal),
-        CUIL_Cifrado = EncryptByPassPhrase(@FraseClave, CUIL);
+    EXEC sp_executesql @sql;
 END;
 GO
 
+--Archivo Informacion_Complementaria / Sucursales:
 CREATE OR ALTER PROCEDURE Procedimientos.CargarSucursales
     @direccion VARCHAR(100),
-    @tabla VARCHAR(100),
-    @pagina VARCHAR(100),
-    @esquema VARCHAR(20)
+    @pagina VARCHAR(100)
 AS
 BEGIN
     DECLARE @sql NVARCHAR(MAX);
-    -- Inserta los datos
+
     SET @sql = N'
-    INSERT INTO ' + QUOTENAME(@esquema) + N'.' + QUOTENAME(@tabla) + N' (Ciudad, ReemplazarPor, Direccion, Horario, Telefono)
+    INSERT INTO Complementario.Sucursales (Ciudad, ReemplazarPor, Direccion, Horario, Telefono)
     SELECT 
         Ciudad,
         [Reemplazar por] AS ReemplazarPor,
